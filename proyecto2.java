469703e6a48ff4251414861c748e96e1a7e054a6
dimasguzman <67974116+dimasguzman@users.noreply.github.com>
2020-07-08 15:20:50 -0600

    Add files via upload

---
+++ proyecto2.java
@@ -0,0 +1,36 @@
+
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Dimas
+ */
+public class proyecto2 {
+
+   
+    public static void main(String[] args) {
+        double num1=15;
+        double num2=30;
+        double suma, resta, multi, mod,division=0 ;
+        suma= num1 + num2;
+        resta= num1 - num2;
+        multi= num1 * num2;
+         mod= num1% num2;
+        
+        if (num2!=0){
+            division= num1 / num2;
+            
+        }
+        
+         System.out.println("La suma es: " + suma);
+ System.out.println("La resta es: " + resta);
+ System.out.println("La multiplicacion es: " + multi);
+  System.out.println("la respuesta de mod es: " + mod);
+ System.out.println("La division es: " + division);
+        
+    }
+    
+}
+     
+    
