c9b66732e57655332ae8479017e23da492bfa67e
dimasguzman <67974116+dimasguzman@users.noreply.github.com>
2020-07-08 15:32:30 -0600

    Add files via upload

---
+++ arreglomultidimensional.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.mavenproject5;
+
+/**
+ *
+ * @author Dimas
+ */
+public class arreglomultidimensional {
+
+    public static void main(String[] args) {
+       
+        
+         {
+   int a[][][][];
+   String[] nombre= new String[5];
+   
+   nombre[0] = "Daniel Medina    Electronica	 TEST  ";
+   nombre[1] = "Monica   Jiz	    Computacion	 IMSA  ";
+   nombre[2] = "Magdaly  varaona   Electronica   sin trbajo";
+   nombre[3] = "Carlos   David     Industrial    IMSA";
+   nombre[4]=  "Jorge    antonio   Computacion   TEST";
+   
+   for(int i=0;i<=5;i++) {  
+     int resultado = i + 1;
+            System.out.println(resultado + ". " + nombre[i]);
+       }
+   
+      }
+        
+     }
+    }
