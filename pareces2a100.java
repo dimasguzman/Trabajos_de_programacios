759e468e7d517db3e63970fa26e2e256b6a6d869
dimasguzman <67974116+dimasguzman@users.noreply.github.com>
2020-07-08 15:30:51 -0600

    Add files via upload

---
+++ pareces2a100.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.pareces2a100;
+
+/**
+ *
+ * @author Dimas
+ */
+import java.util.Scanner;
+public class pareces2a100 {
+    
+    public static void main(String[] args){
+        
+        int k;
+        k = 2;
+        Scanner teclado=new Scanner(System.in);
+        
+        for( k=2;k>=100;k+=2)
+        {
+            System.out.println(k);
+        }
+    }
+
+    
+    
+    
+    
+    
+    
+}
